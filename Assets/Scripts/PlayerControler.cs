434ebfd52abc197dd7e019fde1b6080dd5678ac6
Aletuno <alejandroaceytunogonzalez@gmail.com>
2024-03-06 00:25:10 +0100

    Provisional Camera & Player Controller
    We might want to use Isometrric tilemap for our game for easier programming and asset implementation, so this controler is very basic.
    
    For now, it works for assets

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb.velocity = movement * moveSpeed;
+
+        if (horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
