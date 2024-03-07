50edb003b22f6d13fba9e7e3e9d9e8e87087e0db
Billy <wmoore2024@gmail.com>
2024-03-07 19:50:45 +0400

    Nutrient Bar prototype added
    added the prototype for the nutrient bar

---
+++ Assets/Scripts/NutrientBarSlider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NutrientBarSlider : MonoBehaviour
+{
+    [SerializeField]
+    private Slider nutrientBarSlider;
+    
+    public void SetNutrientBarSlider(int nutrientAmount, int maxNutrients)
+    {
+        nutrientBarSlider.maxValue = maxNutrients;
+        nutrientBarSlider.value = nutrientAmount;
+    }
+}
