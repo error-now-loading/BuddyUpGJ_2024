9e57b490a3bc6c87c35a2454dc3aa6cd0972c5ea
Aletuno <alejandroaceytunogonzalez@gmail.com>
2024-03-07 14:37:00 +0100

    Interactable BaseClass Done

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,15 +6,19 @@ public class Interactable : MonoBehaviour
 {
     [SerializeField] private bool cursorRange = true;
     [SerializeField] private Outline outliner;
+
+    private bool interactable = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (cursorRange && collision.GetComponent<CursorRangeTrigger>())
         {
             outliner.enabled = true;
+            interactable = true;
         }
         else if (!cursorRange && collision.GetComponent<NearRangeTrigger>())
         {
             outliner.enabled = true;
+            interactable = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -22,10 +26,40 @@ public class Interactable : MonoBehaviour
         if (cursorRange && collision.GetComponent<CursorRangeTrigger>())
         {
             outliner.enabled = false;
+            interactable = false;
         }
         else if (!cursorRange && collision.GetComponent<NearRangeTrigger>())
         {
             outliner.enabled = false;
+            interactable = false;
+        }
+    }
+    private void OnMouseEnter()
+    {
+        if (cursorRange)
+        {
+            outliner.SetHover(true);
+        }
+    }
+    private void OnMouseExit()
+    {
+        if (cursorRange)
+        {
+            outliner.SetHover(false);
+        }
+    }
+    private void OnMouseDown()
+    {
+        if (cursorRange && interactable)
+        {
+            Debug.Log("Interacted, must inherit this class for behaviour when interacted");
+        }
+    }
+    public void NearInteraction()
+    {
+        if (!cursorRange && interactable)
+        {
+            Debug.Log("Interacted near, must inherit this class for behaviour when interacted");
         }
     }
 }
--- Assets/Scripts/Outline.cs
+++ Assets/Scripts/Outline.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,34 @@ using UnityEngine;
 public class Outline : MonoBehaviour
 {
     [SerializeField] private Material outlineMat;
+    [SerializeField] private Material outlineHoverMat;
 
     private Material defaultMat;
     private SpriteRenderer spriteRenderer;
+
+    private bool hover = false;
 
     private void OnEnable()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         defaultMat = spriteRenderer.material;
-        spriteRenderer.material = outlineMat;
+        UpdateMat();
     }
     private void OnDisable()
     {
         spriteRenderer.material = defaultMat;
+    }
+    public void SetHover(bool hover)
+    {
+        this.hover = hover;
+        UpdateMat();
+    }
+
+    private void UpdateMat()
+    {
+        if (enabled)
+        {
+            spriteRenderer.material = hover ? outlineHoverMat : outlineMat;
+        }
     }
 }
