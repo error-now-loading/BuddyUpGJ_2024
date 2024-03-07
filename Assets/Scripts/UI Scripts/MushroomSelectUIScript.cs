3db48ecebdb12337d2b8f27ea1c2cc1f9c4e84b3
Billy <wmoore2024@gmail.com>
2024-03-07 19:10:09 +0400

    Mushroom selection proto UI done
    finished prototype of mushroom select UI

--- Assets/Scripts/UI Scripts/MushroomSelectUIScript.cs
+++ Assets/Scripts/UI Scripts/MushroomSelectUIScript.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 public class MushroomSelectUIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private List<GameObject> mushrooms = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private GameObject mushroomSelectorIndicator;
+
+    private int currentlySelectedMushroomIndex;
+
+    private void Update()
     {
-        
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                currentlySelectedMushroomIndex--;
+                if (currentlySelectedMushroomIndex < 0)
+                {
+                    currentlySelectedMushroomIndex = mushrooms.Count - 1;
+                }
+            }
+            else
+            {
+                currentlySelectedMushroomIndex++;
+                if (currentlySelectedMushroomIndex >= mushrooms.Count)
+                {
+                    currentlySelectedMushroomIndex = 0;
+                }
+            }
+        }
+        mushroomSelectorIndicator.transform.position = mushrooms[currentlySelectedMushroomIndex].transform.position;
     }
 }
