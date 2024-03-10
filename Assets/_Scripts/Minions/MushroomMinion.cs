997175e9b1ac7471a1fea429f625371259c72f12
Aletuno <alejandroaceytunogonzalez@gmail.com>
2024-03-10 20:32:30 +0100

    Bulky is done
    except attack, will be done later

--- Assets/_Scripts/Minions/MushroomMinion.cs
+++ Assets/_Scripts/Minions/MushroomMinion.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class MushroomMinion : MonoBehaviour
 {
     [SerializeField] private MushroomTypeSO mushroomType;
-    [SerializeField] private float maxHP = 100f;
+    [SerializeField] private float health = 100f;
     [SerializeField] private float moveSpeed = 10f;
 
-    private bool standing = false; //Should be true, testing
-    private Vector2 destination;
     private Rigidbody2D rb;
+
+    private bool standingAlone = true;
+    private Vector2 destination;
+
     public bool isDed { private set; get; }         //For Anims
     public bool isCarrying { private set; get; }    //For Anims
-    public event Action onAttack;                   //For Anims
+    public event Action onAttack;                   //For Anims TODO attack logic
 
     void Start()
     {
@@ -21,7 +23,7 @@ public class MushroomMinion : MonoBehaviour
 
     void Update()
     {
-        if (!standing)
+        if (!standingAlone && !isDed)
         {
             GoToDestination();
         }
@@ -34,13 +36,42 @@ public class MushroomMinion : MonoBehaviour
         if (Vector2.Distance(currentPos, destination) > 0.1f)
         {
             rb.velocity = moveDir * moveSpeed;
+            if (moveDir.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (moveDir.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
         }
         else
         {
             rb.velocity = Vector3.zero;
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (standingAlone && collision.GetComponent<NearRangeTrigger>())
+        {
+            JoinPlayer();
+        }
+    }
 
+    public void JoinPlayer()
+    {
+        standingAlone = false;
+        FindObjectOfType<PlayerController>().MinionTroopJoin(this);
+    }
+    public void GetHit(float damage)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            isDed = true;
+            rb.velocity = Vector3.zero;
+        }
+    }
     public void SetDestination(Vector2 destination)
     {
         this.destination = destination;
