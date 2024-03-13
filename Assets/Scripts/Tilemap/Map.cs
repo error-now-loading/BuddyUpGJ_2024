b8d5939f219f22ffc4d4f197dcd22ac615789d4d
Kevin Chao <Raiden152@yahoo.com>
2024-03-13 04:56:17 -0700

    Merging main with current Tilemap Generator foundation

---
+++ Assets/Scripts/Tilemap/Map.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : SceneSingleton<Map>
+{
+    private enum TileType : int
+    {
+        Terrain = 0,
+        Decoration = 1
+    }
+
+    private enum TerrainType : int
+    {
+        LongGrass = 0,
+        Muddy = 1
+    }
+
+    [SerializeField] private int mapWidth = 10;
+    [SerializeField] private int mapHeight = 10;
+    [SerializeField] private float mapScale = 5f;
+    [SerializeField] private int mapSmoothness = 5;
+
+    [Header("Terrain Tiles")]
+    [Tooltip("Number of tiles in list must be equal to number of TerrainTypes, order must match order in enum")]
+    [SerializeField] private List<Tile> terrainTiles = null;
+
+    [SerializeField] private Tilemap terrainMap = null;
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (terrainTiles.Count != Enum.GetNames(typeof(TerrainType)).Length)
+        {
+            Debug.LogError("[Map Generation Error]: Number of Tiles in terrainTiles not equal to number of terrain types");
+            return;
+        }
+
+        float[,] noiseMap = PerlinNoiseGenerator.GenerateNoiseMap(mapWidth, mapHeight, mapScale, mapSmoothness, Vector2.zero);
+        float[,] normalizedMap = NormalizeBiome(noiseMap);
+        GenerateTerrainMap(normalizedMap);
+    }
+
+    /// <summary>
+    ///     Normalize noise map for biomes
+    /// </summary>
+    /// <param name="biomeMap">Noise map of the biome</param>
+    /// <returns>Normalized biome map</returns>
+    private float[,] NormalizeBiome(float[,] biomeMap)
+    {
+        int width = biomeMap.GetLength(0);
+        int height = biomeMap.GetLength(1);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (biomeMap[x, y] > 0.5f)
+                {
+                    biomeMap[x, y] = (int)TerrainType.LongGrass;
+                }
+
+                else
+                {
+                    biomeMap[x, y] = (int)TerrainType.Muddy;
+                }
+            }
+        }
+
+        return biomeMap;
+    }
+
+    /// <summary>
+    ///     Generates Tilemap object containing terrain data from provided normalized noise map
+    /// </summary>
+    /// /// <param name="normalizedBiomeMap">Tilemap object representing terrain factoring in biomes</param>
+    /// <returns>Tilemap object representing provided map</returns>
+    private void GenerateTerrainMap(float[,] normalizedBiomeMap)
+    {
+        
+
+    }
+}
---
+++ Assets/Scripts/Utility/PerlinNoiseGenerator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+/// <summary>
+///     Utility class for noise map generation
+/// </summary>
+public static class PerlinNoiseGenerator
+{
+    /// <summary>
+    ///     Generates 2D Perlin Noise matrix
+    /// </summary>
+    /// <param name="width">Matrix width in pixels</param>
+    /// <param name="height">Matrix height in pixels</param>
+    /// <param name="scale">"Zoom" level in Perlin Noise, larger results in smaller groupings</param>
+    /// <param name="octaves">"Smoothness" level in Perlin Noise</param>
+    /// <param name="position">Point of generation</param>
+    /// <returns>2D float[,] matrix</returns>
+    public static float[,] GenerateNoiseMap(int width, int height, float scale, int octaves, Vector2 position)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = (float)x / width * scale;
+                float sampleY = (float)y / width * scale;
+
+                float noise = 0f;
+                float frequency = 0f;
+                for (int oct = 1; oct < octaves; oct *= 2)
+                {
+                    frequency += 1f / oct;
+                    noise += (1f / oct) * Mathf.PerlinNoise(oct * (sampleX + (position.x * scale)),
+                                                            oct * (sampleY + (position.y * scale)));
+                }
+
+                noise /= frequency;
+                noiseMap[x, y] = noise;
+            }
+        }
+
+        return noiseMap;
+    }
+}
