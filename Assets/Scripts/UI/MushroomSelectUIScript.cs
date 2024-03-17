0b4bdb49563323215c7b92a6682c1d787bb86a8a
Billy <wmoore2024@gmail.com>
2024-03-17 17:46:02 +0400

    UIUpdate#1
    I started work on the new mushroom select. Right now all you can do is use num keys 4-9 to set the number of troops for each mushroom type in the ui and the troop icon will disappear when there are no troops of that type

--- Assets/Scripts/UI/MushroomSelectUIScript.cs
+++ Assets/Scripts/UI/MushroomSelectUIScript.cs
@@ -1,37 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MushroomSelectUIScript : MonoBehaviour
 {
-    //Proabably need to change to Image objects later
     [SerializeField]
-    private List<RawImage> mushrooms = new List<RawImage>();
-
+    private List<GameObject> mushroomIcons = new List<GameObject>();
     [SerializeField]
-    private RawImage mushroomSelectorIndicator;
-
-    private int currentlySelectedMushroomIndex;
+    private List<GameObject> keyBinds = new List<GameObject>();
 
     private void Update()
     {
-        if (Input.mouseScrollDelta.y != 0)
+        // Code for testing updatingg input
+        /*if (Input.GetKeyDown("4"))
         {
-            if (Input.mouseScrollDelta.y < 0)
-            {
-                currentlySelectedMushroomIndex--;
-                if (currentlySelectedMushroomIndex < 0)
-                {
-                    currentlySelectedMushroomIndex = mushrooms.Count - 1;
-                }
-            }
-            else
-            {
-                currentlySelectedMushroomIndex = (currentlySelectedMushroomIndex + 1) % mushrooms.Count;
-            }
+            UpdateTroopNumbersUI(0, 1);
         }
-        mushroomSelectorIndicator.transform.position = mushrooms[currentlySelectedMushroomIndex].transform.position;
+        if (Input.GetKeyDown("5"))
+        {
+            UpdateTroopNumbersUI(1, 1);
+        }
+        if (Input.GetKeyDown("6"))
+        {
+            UpdateTroopNumbersUI(2, 1);
+        }
+        if (Input.GetKeyDown("7"))
+        {
+            UpdateTroopNumbersUI(0, 0);
+        }
+        if (Input.GetKeyDown("8"))
+        {
+            UpdateTroopNumbersUI(1, 0);
+        }
+        if (Input.GetKeyDown("9"))
+        {
+            UpdateTroopNumbersUI(2, 0);
+        }*/
+    }
+
+    public void UpdateTroopNumbersUI(int mushroomIndex, int mushroomCount)
+    {
+        mushroomIcons[mushroomIndex].transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().SetText(mushroomCount.ToString());
+        if (mushroomCount > 0)
+        {
+            mushroomIcons[mushroomIndex].SetActive(true);
+        }
+        else
+        {
+            mushroomIcons[mushroomIndex].SetActive(false);
+        }
     }
 }
