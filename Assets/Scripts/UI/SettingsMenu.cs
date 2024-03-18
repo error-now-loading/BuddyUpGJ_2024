d82fedcdff582268a40168217e4bc684e60774b6
Kevin Chao <Raiden152@yahoo.com>
2024-03-18 08:47:34 -0700

    Implementing art assets for SettingsMenu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -10,13 +10,7 @@ using UnityEngine.UI;
 /// </summary>
 public class SettingsMenu : MenuBase
 {
-    [Header("Submenu Canvas Groups")]
-    [SerializeField] private CanvasGroup videoSubMenuCG = null;
-    [SerializeField] private CanvasGroup audioSubMenuCG = null;
-
     [Header("Button References")]
-    [SerializeField] private Button videoButton = null; // VIDEO SETTINGS NOT CURRENTLY IMPLEMENTED
-    [SerializeField] private Button audioButton = null;
     [SerializeField] private Button returnButton = null;
 
     [Header("TextSlider References")]
@@ -33,21 +27,9 @@ public class SettingsMenu : MenuBase
     {
         base.Init();
 
-        videoSubMenuCG.alpha = 0f;
-        audioSubMenuCG.alpha = 0f;
-
-        audioButton.onClick.AddListener(() =>
-        {
-            OpenAudioSubMenu();
-        } );
-
         returnButton.onClick.AddListener(() =>
         {
-            // If audio submenu is open when return button is pressed, fade it out
-            if (audioSubMenuOpen && audioSubMenuCG.alpha > 0f)
-            {
-                FadeOut(audioSubMenuCG, fadeDuration, fadeStartDelay, EaseType.linear, () => { audioSubMenuOpen = false; });
-            }
+            AudioManager.instance.PlaySFX(AudioManager.instance.sourceSFX, AudioManager.instance.uiButtonClick);
 
             FadeOut(mainCG, fadeDuration, fadeStartDelay, EaseType.linear, () =>
             {
@@ -62,34 +44,6 @@ public class SettingsMenu : MenuBase
         LoadAudioSliderSettings();
     }
 
-    public void OpenAudioSubMenu()
-    {
-        if (audioSubMenuOpen)
-        {
-            CloseAudioSubMenu();
-            return;
-        }
-        audioSubMenuOpen = true;
-
-        FadeIn(audioSubMenuCG, fadeDuration, fadeStartDelay, EaseType.linear);
-    }
-
-    public void CloseAudioSubMenu()
-    {
-        if (audioSubMenuOpen)
-        {
-            FadeOut(audioSubMenuCG, fadeDuration, fadeStartDelay, EaseType.linear);
- 
-            audioSubMenuOpen = false;
-        }
-
-        else
-        {
-            Debug.Log($"[SettingsMenu Error]: Audio SubMenu is not opened");
-            return;
-        }
-    }
-
     private void LoadAudioSliderSettings()
     {
         masterVol.SetSliderValue(SaveDataUtility.LoadFloat(SaveDataUtility.MASTER_VOLUME, 0.5f));
@@ -100,24 +54,23 @@ public class SettingsMenu : MenuBase
     private void OnMasterVolChanged(float argValue)
     {
         SaveDataUtility.SaveFloat(SaveDataUtility.MASTER_VOLUME, argValue);
-        //AudioManager.instance.LoadMasterVolume();
+        AudioManager.instance.LoadMasterVolume();
     }
 
     private void OnSFXVolChanged(float argValue)
     {
         SaveDataUtility.SaveFloat(SaveDataUtility.SFX_VOLUME, argValue);
-        //AudioManager.instance.LoadSFXVolume();
+        AudioManager.instance.LoadSFXVolume();
     }
     
     private void OnMusicVolChanged(float argValue)
     {
         SaveDataUtility.SaveFloat(SaveDataUtility.MUSIC_VOLUME, argValue);
-        //AudioManager.instance.LoadMusicVolume();
+        AudioManager.instance.LoadMusicVolume();
     }
 
     private void OnDestroy()
     {
-        audioButton.onClick.RemoveAllListeners();
         returnButton.onClick.RemoveAllListeners();
     }
 }
