74bd104b7356903ed9a7c6828cb5563e97f62b95
Kevin Chao <Raiden152@yahoo.com>
2024-03-20 06:59:03 -0700

    Merging GameScene with copy on main, removing erroneous destroy call in PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -255,7 +255,6 @@ public class PlayerController : MonoBehaviour
             rb.velocity = Vector3.zero;
 
             EventManager.instance.Notify(EventTypes.PlayerDeath);
-            Destroy(gameObject);
         }
     }
 }
