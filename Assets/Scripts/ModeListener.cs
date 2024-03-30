aa0b349e16429ed1b13c7f1d15b2e3ba6f37573d
Kevin Chao <Raiden152@yahoo.com>
2024-03-30 14:06:33 -0700

    Finishing implementation of ModeListener

--- Assets/Scripts/ModeListener.cs
+++ Assets/Scripts/ModeListener.cs
@@ -1,52 +1,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class ModeListener : SceneSingleton<ModeListener>
 {
-    private List<char> codeStorage = null;
-    private string passcode = "SHROOLOO5EVER";
+    private List<Key> codeStorage = new List<Key>() { Key.S, Key.H, Key.R, Key.O, Key.O, Key.L, Key.O, Key.O, Key.Digit5, Key.E, Key.V, Key.E, Key.R };
+    private readonly List<Key> passcode = new List<Key>() { Key.S, Key.H, Key.R, Key.O, Key.O, Key.L, Key.O, Key.O, Key.Digit5, Key.E, Key.V, Key.E, Key.R };
+    private bool codeEntered = false;
 
 
 
     private void Start()
     {
-        codeStorage = new List<char>();
+        codeStorage = new List<Key>();
     }
 
     private void Update()
     {
-        HandleModeSwitch();
+        if (!codeEntered)
+        {
+            HandleModeSwitch();
+        }
     }
 
     private void HandleModeSwitch()
     {
-        foreach (var key in Keyboard.current.allKeys)
+        for (int i = 0; i < Keyboard.current.allKeys.Count; i++)
         {
-            if (key.wasPressedThisFrame)
+            KeyControl curKey = Keyboard.current.allKeys[i];
+            if (curKey.wasPressedThisFrame)
             {
-                Debug.Log($"{key.keyCode}");
+                AddKeyToBuffer(curKey);
             }
+        }
+
+        if (IsCodeEntered())
+        {
+            // Activate Shrooloo mode flag
+            SaveDataUtility.SaveBool(SaveDataUtility.SHROOLOO_MODE_KEY, true);
+
+            codeEntered = true;
+        }
+    }
+
+    private void AddKeyToBuffer(KeyControl curKey)
+    {
+        codeStorage.Add(curKey.keyCode);
+
+        // Maintain buffer length
+        if (codeStorage.Count > passcode.Count)
+        {
+            codeStorage.RemoveAt(0);
         }
     }
 
     private bool IsCodeEntered()
     {
-        if (codeStorage.Count != passcode.Length)
+        if (codeStorage.Count != passcode.Count)
         {
             return false;
         }
 
-        else
+        for (int i = 0; i < passcode.Count; i++)
         {
-            string checker = string.Concat(codeStorage);
-
-            if (checker == passcode)
+            if (codeStorage[i] != passcode[i])
             {
-                return true;
+                return false;
             }
-
-            return false;
         }
+
+        return true;
     }
 }
